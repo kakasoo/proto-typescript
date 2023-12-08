bf408fcd8ce32d7b1e2e50b11d4f535589baaca4
kakasoo <kscodebase@gmail.com>
2023-12-08 23:28:32 +0900

    feat : more type-safe `StringPrototype.split` method by adding Limit type

--- src/prototypes/string.prototype.ts
+++ src/prototypes/string.prototype.ts
@@ -1,1 +1,11 @@
-export namespace StringPrototype {}
+import { Split } from '../types';
+
+export namespace StringPrototype {
+  export function split<Container extends string, Splitter extends string, Limit extends number>(
+    container: Container,
+    splitter: Splitter,
+    limit?: Limit,
+  ): Split<Container, Splitter, Limit> {
+    return container.split(splitter, limit) as Split<Container, Splitter, Limit>;
+  }
+}
