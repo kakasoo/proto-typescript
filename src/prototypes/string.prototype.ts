df3f6acb2a2d6f28f2790a69f3c33de6ec35d5a4
kakasoo <kscodebase@gmail.com>
2023-12-08 23:58:03 +0900

    fix : StringPrototype modifed form namespace to variable

--- src/prototypes/string.prototype.ts
+++ src/prototypes/string.prototype.ts
@@ -1,11 +1,11 @@
 import { Split } from '../types';
 
-export namespace StringPrototype {
-  export function split<Container extends string, Splitter extends string, Limit extends number>(
+export const StringPrototype = {
+  split<Container extends string, Splitter extends string, Limit extends number>(
     container: Container,
     splitter: Splitter,
     limit?: Limit,
   ): Split<Container, Splitter, Limit> {
     return container.split(splitter, limit) as Split<Container, Splitter, Limit>;
-  }
-}
+  },
+};
