eb87bd83c553b52e37d7df0dd0f6c2093df0a9ee
kakasoo <kscodebase@gmail.com>
2023-12-14 23:22:15 +0900

    feat : TypedNumber's ValueType, ValueTypes defined

--- src/typed-number.class.ts
+++ src/typed-number.class.ts
@@ -1,5 +1,6 @@
 import { toPrimitive } from './interfaces/to-primitive.interface';
 import { TypedObject } from './typed-object.class';
+import { ReadonlyOrNot } from './types/primitive.type';
 
 export class TypedNumber<T extends number = 0> extends TypedObject<T> implements toPrimitive<T> {
   constructor(private readonly number: T = 0 as T) {
@@ -9,4 +10,25 @@ export class TypedNumber<T extends number = 0> extends TypedObject<T> implements
   toPrimitive(): T {
     return this.number;
   }
+}
+
+export namespace TypedNumber {
+  /**
+   * Inference of value type.
+   */
+  export type ValueType<Pointer extends TypedNumber<any> | number> = Pointer extends TypedNumber<infer T>
+    ? T
+    : Pointer extends number
+      ? Pointer
+      : never;
+
+  /**
+   * Inference of value types.
+   */
+  export type ValueTypes<Pointers extends ReadonlyOrNot<(TypedNumber<any> | number)[]>> = Pointers extends [
+    infer F extends TypedNumber<infer T> | number,
+    ...infer Rest extends ReadonlyOrNot<(TypedNumber<any> | number)[]>,
+  ]
+    ? [TypedNumber.ValueType<F>, ...TypedNumber.ValueTypes<Rest>]
+    : [];
 }
