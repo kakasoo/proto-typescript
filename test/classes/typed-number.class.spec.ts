72119215fdee1886b865caa393f5f2e9d452b600
kakasoo <kscodebase@gmail.com>
2023-12-22 01:26:45 +0900

    test : test case for TypedInt, TypedDecimal #1

--- test/classes/typed-number.class.spec.ts
+++ test/classes/typed-number.class.spec.ts
@@ -1,5 +1,5 @@
 import typia from 'typia';
-import { NumberType, StringType } from '../../src';
+import { NumberType, StringType, TypedNumber } from '../../src';
 
 describe('TypedInt', () => {
   it('Int TypedInt class constructor can not receive non-int value.', async () => {
@@ -11,5 +11,27 @@ describe('TypedInt', () => {
 
     type errorMessage = StringType.IsInt<num>;
     expect(typia.random<errorMessage>()).toBe('IS NOT INT FORMAT');
+  });
+});
+
+describe('TypedNumber', () => {
+  it('Decimal, integer generic type omitted by using functional parameter.', async () => {
+    const case3 = TypedNumber.refine('decimal', 1, 2).Decimal(3.14);
+    expect(case3.toPrimitive()).toBe(3.14);
+  });
+
+  it('Integer and Decimal specification can be made using generics.', async () => {
+    const case1 = TypedNumber.refine<1, 2>('decimal').Decimal(3.14);
+    expect(case1.toPrimitive()).toBe(3.14);
+  });
+
+  it('if you don not set decimal, integer length...', async () => {
+    const case1 = TypedNumber.refine('decimal').Decimal(3.14);
+    expect(case1.toPrimitive()).toBe(3.14);
+  });
+
+  it('TypedInt can be called from TypedNumber', async () => {
+    const case1 = TypedNumber.refine('int').Int(3);
+    expect(case1.toPrimitive()).toBe(3);
   });
 });
