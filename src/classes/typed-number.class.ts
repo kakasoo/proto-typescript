b86629d5f57bbf061d5b43763f99b4fc710be9f0
kakasoo <kscodebase@gmail.com>
2023-12-22 01:29:41 +0900

    feat : TypedNumber's refined method created for `TypedInt`, `TypedDecimal` #1

--- src/classes/typed-number.class.ts
+++ src/classes/typed-number.class.ts
@@ -1,5 +1,8 @@
 import { toPrimitive } from '../interfaces/to-primitive.interface';
+import { StringType } from '../types';
 import { ReadonlyOrNot } from '../types/primitive.type';
+import { TypedDecimal } from './numbers/typed-decimal.class';
+import { TypedInt } from './numbers/typed-int.class';
 import { TypedObject } from './typed-object.class';
 
 export class TypedNumber<T extends number> extends TypedObject<T> implements toPrimitive<T> {
@@ -8,6 +11,40 @@ export class TypedNumber<T extends number> extends TypedObject<T> implements toP
   constructor(data = 0 as T) {
     super(data);
     this.number = data;
+  }
+
+  static refine<Integer extends number>(
+    format: 'int',
+  ): { Int: <N extends number>(data: StringType.IsInt<N>) => TypedInt<N> };
+
+  static refine<Integer extends number, Fractional extends number>(
+    format: 'decimal',
+    integer?: Integer,
+    fractional?: Fractional,
+  ): {
+    Decimal: <N extends number>(
+      data: StringType.IsDecimal<N, Integer, Fractional>,
+    ) => TypedDecimal<N, Integer, Fractional>;
+  };
+
+  static refine<Integer extends number, Fractional extends number>(
+    format: 'int' | 'decimal',
+    integer?: Integer,
+    fractional?: Fractional,
+  ) {
+    if (format === 'int') {
+      const Int = <N extends number>(data: StringType.IsInt<N>): TypedInt<N> => {
+        return new TypedInt<N>(data);
+      };
+
+      return { Int };
+    } else {
+      const Decimal = <N extends number>(data: StringType.IsDecimal<N, Integer, Fractional>) => {
+        return new TypedDecimal<N, Integer, Fractional>(data);
+      };
+
+      return { Decimal };
+    }
   }
 
   toPrimitive(): T {
