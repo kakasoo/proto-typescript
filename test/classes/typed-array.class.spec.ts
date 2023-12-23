50a1ca84a99095cceff01e47d201add10e4116ab
kakasoo <kscodebase@gmail.com>
2023-12-23 19:19:59 +0900

    fix : fix typo for error namespace #1

--- test/classes/typed-array.class.spec.ts
+++ test/classes/typed-array.class.spec.ts
@@ -1,14 +1,16 @@
 import { TypedArray } from '../../src';
 
 /**
- * errors
+ * error case
  */
-new TypedArray<'1-3'>([1, 2, 3]).refine([1, 2, 3] as const).join('-');
 new TypedArray<'1-3'>([1, 2, 3]).refine([1, 2, 3] as const).join('-');
 new TypedArray<'1-3'>([]).refine([1, 2, 3] as const).join('-');
 new TypedArray<'1-3'>().refine([1, 2, 3, 4] as const).join('-');
 new TypedArray<'1-3'>().refine([0, 1, 2, 3] as const).join('-');
 
+/**
+ * right case
+ */
 new TypedArray(1, 2, 3);
 new TypedArray(1, 2, 3 as const);
 new TypedArray([1, 2, 3, 4] as const);
