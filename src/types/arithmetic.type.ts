f5e42bad1fb5357ddf3859a5008a4456c38eb207
kakasoo <kscodebase@gmail.com>
2023-12-24 23:16:34 +0900

    fix : MathType defined (implementing...)

--- src/types/arithmetic.type.ts
+++ src/types/arithmetic.type.ts
@@ -1,4 +1,5 @@
 import { ArrayType } from './array.type';
+import { NumberType } from './number.type';
 import { StringType } from './string.type';
 
 export interface StringToNumberMap {
@@ -6,11 +7,25 @@ export interface StringToNumberMap {
   '1': 1;
   '2': 2;
   '3': 3;
+  '4': 4;
   '5': 5;
   '6': 6;
   '7': 7;
   '8': 8;
   '9': 9;
+}
+
+export interface CeilingMap {
+  '0': '1';
+  '1': '2';
+  '2': '3';
+  '3': '4';
+  '4': '5';
+  '5': '6';
+  '6': '7';
+  '7': '8';
+  '8': '9';
+  '9': '0'; // if this...
 }
 
 /**
@@ -30,12 +45,76 @@ export type StringToNumberStringTuple<T extends string> = T extends `${infer F}$
     : never
   : [];
 
-/**
- * 숫자와 문자 타입이 서로 오갈 수 있게 보조하는 두 타입
- */
-export type NumberToString<N extends number> = `${N}`;
-export type StringToNumber<S extends string> = S extends NumberToString<infer P> ? P : never;
-
 export type Falsy = false | 0 | '' | null | undefined;
 export type Truthy<T> = T extends Falsy ? false : true;
 export type Conditional<Condition, T, F> = Truthy<Condition> extends true ? T : F;
+
+export namespace Math {
+  type _FindLastCeilTarget<T extends `${number}`[], IndexFromLast extends number = 0> = T extends [
+    ...infer Rest extends (keyof CeilingMap)[],
+    infer BeforeLast extends keyof CeilingMap,
+    infer Last,
+  ]
+    ? CeilingMap[BeforeLast] extends '0'
+      ? _FindLastCeilTarget<[...Rest, BeforeLast], NumberType.Add<IndexFromLast, 1>>
+      : IndexFromLast
+    : never;
+
+  /**
+   * Throw away the last digit and add 1 to the next digit.
+   *
+   * @example
+   * _Ceil<'230'> // 24
+   * _Ceil<'231'> // 24
+   * _Ceil<'232'> // 24
+   * _Ceil<'233'> // 24
+   * _Ceil<'234'> // 24
+   * _Ceil<'235'> // 24
+   * _Ceil<'236'> // 24
+   * _Ceil<'237'> // 24
+   * _Ceil<'238'> // 24
+   * _Ceil<'239'> // 24
+   */
+  type _Ceil<N extends string> = StringType.Split<N> extends [
+    ...infer Rest extends (keyof CeilingMap)[],
+    infer BeforeLast extends keyof CeilingMap,
+    infer Last extends keyof CeilingMap,
+  ]
+    ? CeilingMap[BeforeLast] extends '0'
+      ? _Ceil<StringType.ThrowAway<N, _FindLastCeilTarget<StringType.Split<N>>>>
+      : ArrayType.Join<[...Rest, CeilingMap[BeforeLast]], ''>
+    : '';
+
+  /**
+   * 여기서 말하는 N은 소숫점 이하의 숫자들임에 주의하라.
+   */
+  export type CeilFractional<N extends string, Length extends number = StringType.Length<N>> = StringType.PadEnd<
+    _Ceil<N>,
+    Length,
+    '0'
+  >;
+
+  /**
+   * Enter a number and round it to a certain digit.
+   * If there is no decimal point, the original number must be returned without rounding.
+   */
+  export type _Round<N extends number, TargetDigit extends number> = N extends StringType.IsDecimal<N, number, number>
+    ? StringType.InsertedFractional<N> extends string
+      ? StringType.At<StringType.InsertedFractional<N>, TargetDigit> extends '5' | '6' | '7' | '8' | '9'
+        ? `${StringType.InsertedInteger<N>}.${CeilFractional<StringType.InsertedFractional<N>, TargetDigit>}`
+        : /**
+           * @example
+           * type a3 = Math.Round<1.39946, 3>; // '1.400'
+           */
+          `${StringType.InsertedInteger<N>}.${StringType.ThrowAway<
+            StringType.InsertedFractional<N>,
+            NumberType.Sub<TargetDigit, 2>
+          >}`
+      : N
+    : N;
+
+  export type Round<N extends number, TargetDigit extends number> = _Round<
+    StringType.ToNumber<StringType.Take<`${N}`, NumberType.Add<TargetDigit, 3>>>,
+    TargetDigit
+  >;
+}
